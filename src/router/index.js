72406795197dfd0ec607ca0f33438ee5c3fbfba0
tangxiaohui <912820339@qq.com>
2018-05-03 17:32:17 +0800

    Merge branch 'develop' of http://git.dev.anoah.com/yanglin21/yxban-js-teacher into common
    # Conflicts:
    #	src/router/index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,19 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import account from '@/common/account/router'
-import userCenter from '@/common/user-center/router'
-import home from '@/components/home/index'
+import correction from '@/common/correction/router'
+import notice from '@/common/notice/router'
+import Home from '@/components/home'
 
 Vue.use(Router)
-let routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: home,
-    meta: { requiresAuth: true, keepAlive: false }
-  }
-]
 
-routes = routes.concat(account).concat(userCenter)
-
+let routes = [{
+  path: '/',
+  name: 'home',
+  component: Home,
+  meta: { requiresAuth: true, keepAlive: true }
+}]
+routes = routes.concat(account,correction,notice)
 let router = new Router({
   routes: routes
 })
