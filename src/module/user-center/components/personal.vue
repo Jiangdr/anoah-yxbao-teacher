577f30e574339488e1c983312783bd6a7503b196
rigeman <283082036@qq.com>
2018-05-03 18:16:48 +0800

    5.3新增

--- src/module/user-center/components/personal.vue
+++ src/module/user-center/components/personal.vue
@@ -3,23 +3,23 @@
     <nav-bar :title="title" :hasBack="hasBack" @historyBack="back"></nav-bar>
     <div class="item-choice van-hairline--bottom" @click="modifyIcon">
       <span>头像</span>
-      <img src="/uploads/users/45/26/289385.jpg" class="avatar van-hairline--bottom-top-left-right" alt="">
+      <img :src="userInfo.avatar" class="avatar van-hairline--bottom-top-left-right" alt="">
     </div>
     <div class="item-choice van-hairline--bottom">
       <span>账号</span>
-      <span>蒲栋</span>
+      <span>{{userInfo.username}}</span>
     </div>
     <router-link class="item-choice van-hairline--bottom" tag="div" :to="{name:'modifyName'}">
       <span>姓名</span>
-      <span><span>rigeman</span><van-icon name="arrow" style="vertical-align:middle;top:-2px"></van-icon></span>
+      <span><span>{{userInfo.real_name}}</span><van-icon name="arrow" style="vertical-align:middle;top:-2px"></van-icon></span>
     </router-link>
     <div class="item-choice van-hairline--bottom">
       <span>学校</span>
-      <span>白沙中学</span>
+      <span>{{userInfo.school_name}}</span>
     </div>
     <router-link class="item-choice van-hairline--bottom" tag="div" :to="{name:'modifyPhone'}">
       <span>手机号</span>
-      <span><span>18081052714</span><van-icon name="arrow" style="vertical-align:middle;top:-2px"></van-icon></span>
+      <span><span>{{userInfo.phone || '请绑定'}}</span><van-icon name="arrow" style="vertical-align:middle;top:-2px"></van-icon></span>
     </router-link>
     <van-actionsheet v-model="showActionSheet" :actions="actions" cancel-text="取消" />
   </div>
@@ -27,6 +27,7 @@
 
 <script>
 import NavBar from '@/module/user-center/components/common/navbar'
+import {mapGetters} from 'vuex'
 export default {
   name: 'Personal',
   data () {
@@ -43,6 +44,11 @@ export default {
         }
       ]
     }
+  },
+  computed: {
+    ...mapGetters({
+      userInfo: 'userCenter/userInfo'
+    })
   },
   methods: {
     modifyIcon () {
