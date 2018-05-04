3e4a209eea101b59fda531eef67026fcd774285c
tangxiaohui <912820339@qq.com>
2018-05-04 10:58:34 +0800

    样式

--- src/router/index.js
+++ src/router/index.js
@@ -3,32 +3,19 @@ import Router from 'vue-router'
 import account from '@/common/account/router'
 import correction from '@/common/correction/router'
 import notice from '@/common/notice/router'
-<<<<<<< HEAD
-import Home from '@/components/home'
-
-Vue.use(Router)
-=======
 import userCenter from '@/common/user-center/router'
 import home from '@/components/home/index'
-
 Vue.use(Router)
-
 let routes = [{
   path: '/',
   name: 'home',
   component: home,
-  meta: { requiresAuth: true, keepAlive: true }
+  meta: {
+    requiresAuth: true,
+    keepAlive: true
+  }
 }]
 routes = routes.concat(account).concat(userCenter).concat(account, correction, notice)
->>>>>>> common
-
-let routes = [{
-  path: '/',
-  name: 'home',
-  component: Home,
-  meta: { requiresAuth: true, keepAlive: true }
-}]
-routes = routes.concat(account,correction,notice)
 let router = new Router({
   routes: routes
 })
@@ -44,7 +31,9 @@ router.beforeEach((to, from, next) => {
       if (!router.app.$options.store.state.account.isLogin) {
         next({
           path: '/login',
-          query: { redirect: to.fullPath }// 把要跳转的地址作为参数传到下一步
+          query: {
+            redirect: to.fullPath
+          } // 把要跳转的地址作为参数传到下一步
         })
       } else {
         next()
@@ -54,10 +43,10 @@ router.beforeEach((to, from, next) => {
     }
   } else {
     // 如果to登录页
-    if (to.name === 'login') {
+    if (to.path === '/login') {
       // 帐号已经登录过了，就直接转到首页
       router.app.$options.store.dispatch('account/refreshLocalIsLogin')
-      console.log('router.app.$options.store.state.account.isLogin', router.app.$options.store.state.account.isLogin)
+      // console.log('router.app.$options.store.state.account.isLogin', router.app.$options.store.state.account.isLogin)
       if (router.app.$options.store.state.account.isLogin) {
         next({
           path: '/'
