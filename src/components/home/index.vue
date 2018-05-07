a73fd5782fa0794a0592431c2a0ec7792cf93266
puronglong <puronglong@foxmail.com>
2018-05-07 14:30:12 +0800

    添加作业模块

--- src/components/home/index.vue
+++ src/components/home/index.vue
@@ -13,7 +13,7 @@
         <van-button type="primary" block>互动课堂</van-button>
       </van-col>
       <van-col span="12">
-        <van-button type="primary" block @click="tt">作业</van-button>
+        <van-button type="primary" block @click="goHomework">作业</van-button>
       </van-col>
       <van-col span="12">
         <van-button type="primary" block @click="change">我的</van-button>
@@ -67,9 +67,9 @@ export default {
       alert(2)
     },
 
-    tt () {
+    goHomework () {
       this.$router.push({
-        path: '/list'
+        path: '/homework'
       })
     },
     onClickLeft () {
---
+++ src/components/homework/homework.vue
@@ -0,0 +1,36 @@
+<template>
+  <cube-button>Button</cube-button>
+</template>
+
+<script>
+export default {
+  name: 'Homework',
+  data () {
+    return {
+      list: [],
+      loading: false,
+      refreshLoading: false,
+      finished: false
+    }
+  },
+  computed: {
+
+  },
+  methods: {
+    goHomework () {
+      this.$router.push({
+        path: '/homework'
+      })
+    },
+    onClickLeft () {
+
+    },
+    onClickRight () {
+
+    }
+  }
+}
+</script>
+<style scoped>
+
+</style>
