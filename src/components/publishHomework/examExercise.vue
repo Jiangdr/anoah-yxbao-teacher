24865cf5734d801e30b804571e8df70a7e0eb3d7
puronglong <puronglong@foxmail.com>
2018-05-11 18:35:21 +0800

    修改试卷页面

--- src/components/publishHomework/examExercise.vue
+++ src/components/publishHomework/examExercise.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="cube-page cube-view button-view">
     <header class="header">
-      <h1>试卷名称</h1>
+      <h1>试卷</h1>
       <i class="cubeic-back" @click="goSummerHomework">返回</i>
     </header>
-    <div style="overflow-y:auto;height:91%;overflow-x:hidden;">
+    <div style="overflow-y:auto;overflow-x:hidden;" v-bind:style="listContainerStyle">
       <div v-for="(value,key) in setting" :key="key">
         <Qti :setting="value"></Qti>
       </div>
@@ -17,7 +17,10 @@ export default {
   name: "examExercise",
   data() {
     return {
-      setting: []
+      setting: [],
+      listContainerStyle: {
+        height: window.innerHeight - 50 + 'px'
+      }
     };
   },
   computed: {},
@@ -55,44 +58,6 @@ export default {
       this.$router.push({
         path: "/homeworkPublishSetting"
       });
-    },
-    checkboxChange() {
-      var array = this.result;
-      this.hasChoosePagesNum = array.length;
-      this.hasChooseProblemsNum = 0;
-      for (let i = 0; i < array.length; i++) {
-        this.hasChooseProblemsNum += Number(array[i].qti_num);
-      }
-    },
-    clickTab(name) {
-      this.activeTabName = name;
-    },
-    clickSelectAll() {
-      this.checkList = [];
-      var array = this.options;
-      for (let i = 0; i < array.length; i++) {
-        this.checkList.push(array[i].value);
-      }
-    },
-    getList: function(value) {
-      var self = this;
-      var url = "jwt/resource/package/getResourceLists?";
-      var data = {
-        user_id: "33737",
-        pack_id: "9002511525420500001"
-      };
-      self.$http
-        .get(url, { params: data })
-        .then(function(response) {
-          if (response.data.msg === "ok") {
-            self.lists = response.data.recordset.lists;
-          } else {
-            console.log(response);
-          }
-        })
-        .catch(function(response) {
-          console.log(response);
-        });
     },
     clickCell() {
       console.log("click cell");
