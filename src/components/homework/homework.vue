35335961b881b3615a3732530b46f824e46e5e3a
puronglong <puronglong@foxmail.com>
2018-05-14 18:30:50 +0800

    修改作业列表筛选条件

--- src/components/homework/homework.vue
+++ src/components/homework/homework.vue
@@ -14,16 +14,18 @@
         <div class="select-span-div" @click="clickClass">班级<i class="fa fa-sort-down"></i></div>
       </span>
       <span class="select-span">
-        <cube-select :options="options" placeholder="状态" title="状态"></cube-select>
+        <!-- <cube-select :options="options" placeholder="状态" title="状态"></cube-select> -->
+        <div class="select-span-div" @click="clickStatus">状态<i class="fa fa-sort-down"></i></div>
       </span>
       <span class="select-span">
-        <cube-select :options="options" placeholder="时间" title="时间"></cube-select>
+        <!-- <cube-select :options="options" placeholder="时间" title="时间"></cube-select> -->
+        <div class="select-span-div" @click="clickTime">时间<i class="fa fa-sort-down"></i></div>
       </span>
     </div>
 
     <div class="listContainer" v-bind:style="listContainerStyle">
       <van-pull-refresh v-model="isLoading" @refresh="onRefresh">
-        <div @click="goHomeworkDetail(item)" class="homework_list" v-for="(item, index) in homeworkListArray" :key="index">
+        <div @click="goHomeworkDetail(item)" class="homework_list" v-for="(item, index) in homeworkListArray" :key="index" v-if="homeworkListArray.length > 0">
           <div>
             <div class="homework_list_inline_list">{{item.start_time}}</div>
             <div class="homework_list_inline_list">{{item.title}}</div>
@@ -31,11 +33,23 @@
             <div class="homework_list_inline_list">截止：{{item.deadline}}</div>
           </div>
         </div>
+
+        <div v-if="homeworkListArray.length === 0" style="height: 200px;line-height: 200px;text-align: center;">
+          没有数据
+        </div>
       </van-pull-refresh>
     </div>
 
-    <van-popup v-model="showPopup" position="bottom" :overlay="true">
-      <van-picker show-toolbar :columns="columns" @cancel="onCancel" @confirm="onConfirm"/>
+    <van-popup v-model="showClassPopup" position="bottom" :overlay="true">
+      <van-picker show-toolbar :columns="columnsOfClass" @cancel="onCancelClassPopup" @confirm="onConfirmClassPopup"/>
+    </van-popup>
+
+    <van-popup v-model="showStatusPopup" position="bottom" :overlay="true">
+      <van-picker show-toolbar :columns="columnsOfStatus" @cancel="onCancelStatusPopup" @confirm="onConfirmStatusPopup"/>
+    </van-popup>
+
+    <van-popup v-model="showTimePopup" position="bottom" :overlay="true">
+      <van-picker show-toolbar :columns="columnsOfTime" @cancel="onCancelTimePopup" @confirm="onConfirmTimePopup"/>
     </van-popup>
 
     <div class="publishHomeworkBtnDiv" @click="goChooseTextbook">
@@ -62,9 +76,70 @@ export default {
       loading: false,
       finished: false,
       isLoading: false,
-      showPopup: false,
-      columns: []
+      showClassPopup: false,
+      showStatusPopup: false,
+      showTimePopup: false,
+      columnsOfClass: [],
+      columnsOfStatus: [
+        {
+          text: '全部',
+          value: 'all'
+        }, {
+          text: '待批改',
+          value: 'correct'
+        }, {
+          text: '已批改',
+          value: 'finish'
+        }
+      ]
     };
+  },
+  created: function() {
+    var nowdate = new Date();
+    var yNow = nowdate.getFullYear();
+    var mNow = nowdate.getMonth() + 1;
+    var dNow = nowdate.getDate();
+    var formatnowdate = yNow + '-' + mNow + '-' + dNow;
+    // $("#enddate").datebox("setValue",formatnowdate);
+    // 获取系统前一周的时间
+    var oneweekdate = new Date(nowdate - 7 * 24 * 3600 * 1000);
+    var yWeek = oneweekdate.getFullYear();
+    var mWeek = oneweekdate.getMonth() + 1;
+    var dWeek = oneweekdate.getDate();
+    var formatWeekdate = yWeek + '-' + mWeek + '-' + dWeek;
+    // $("#fdate").datebox("setValue",formatwdate);
+    // 获取系统前一个月的时间
+    nowdate.setMonth(nowdate.getMonth() - 1);
+    var yOneMonth = nowdate.getFullYear();
+    var mOneMonth = nowdate.getMonth() + 1;
+    var dOneMonth = nowdate.getDate();
+    var formatOneMonthdate = yOneMonth + '-' + mOneMonth + '-' + dOneMonth;
+
+    nowdate.setMonth(nowdate.getMonth() - 2);
+    var yThreeMonth = nowdate.getFullYear();
+    var mThreeMonth = nowdate.getMonth() + 1;
+    var dThreeMonth = nowdate.getDate();
+    var formatThreeMonthdate = yThreeMonth + '-' + mThreeMonth + '-' + dThreeMonth;
+    // $("#fdate").datebox("setValue",formatwdate);
+    this.columnsOfTime = [
+      {
+        text: '全部',
+        from: '',
+        to: ''
+      }, {
+        text: '最近一周',
+        from: formatWeekdate,
+        to: formatnowdate
+      }, {
+        text: '最近一个月',
+        from: formatOneMonthdate,
+        to: formatnowdate
+      }, {
+        text: '最近三个月',
+        from: formatThreeMonthdate,
+        to: formatnowdate
+      }
+    ]
   },
   mounted: function() {
     this.userInfo = this.$store.state.account.userInfo;
@@ -82,26 +157,52 @@ export default {
         }
       );
     }
-    this.columns = array;
-    this.getHomeworkList(this.columns[0].class_id);
+    this.columnsOfClass = array;
+    this.chooseClass = this.columnsOfClass[0];
+    this.chooseStatus = this.columnsOfStatus[0];
+    this.chooseTime = this.columnsOfTime[0];
+    this.getHomeworkList();
   },
   methods: {
     clickClass() {
-      this.showPopup = !this.showPopup;
+      this.showClassPopup = !this.showClassPopup;
     },
-    onConfirm(value, index) {
-      this.showPopup = false;
-      this.getHomeworkList(value.class_id);
+    clickStatus() {
+      this.showStatusPopup = !this.showStatusPopup;
     },
-    onCancel() {
-      this.showPopup = false;
+    clickTime() {
+      this.showTimePopup = !this.showTimePopup;
+    },
+    onConfirmClassPopup(value, index) {
+      this.showClassPopup = false;
+      this.chooseClass = value;
+      this.getHomeworkList();
+    },
+    onCancelClassPopup() {
+      this.showClassPopup = false;
+    },
+    onConfirmStatusPopup(value, index) {
+      this.showStatusPopup = false;
+      this.chooseStatus = value;
+      this.getHomeworkList();
+    },
+    onCancelStatusPopup() {
+      this.showStatusPopup = false;
+    },
+    onConfirmTimePopup(value, index) {
+      this.showTimePopup = false;
+      this.chooseTime = value;
+      this.getHomeworkList();
+    },
+    onCancelTimePopup() {
+      this.showTimePopup = false;
     },
     onRefresh() {
       setTimeout(() => {
         this.$toast("刷新成功");
         this.isLoading = false;
       }, 500);
-      this.getHomeworkList(this.columns[0].class_id);
+      this.getHomeworkList(this.columns[0].class_id, 'all');
     },
     goHomeworkDetail(item) {
       this.$router.push({
@@ -127,8 +228,11 @@ export default {
       var self = this;
       var url = "/jwt/zuoye/homework/homeworkLists?";
       var data = {
-        user_id: this.userInfo.userid,
-        class_id: classId
+        user_id: self.userInfo.userid,
+        class_id: self.chooseClass.class_id,
+        status: self.chooseStatus.value,
+        from: self.chooseTime.from,
+        to: self.chooseTime.to
       };
 
       self.$http
