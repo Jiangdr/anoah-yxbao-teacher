0313131acb7c0c33cc2b8634df562ac640fedb10
tangxiaohui <912820339@qq.com>
2018-05-16 11:19:47 +0800

    查看学生情况

--- src/module/tongji/components/question/correctTable.vue
+++ src/module/tongji/components/question/correctTable.vue
@@ -39,7 +39,7 @@
           </p>
         </van-col>
         <van-col span="6" class="unanswered">
-          <p @click="item.record.noanswer>0?toggleAllCorrec('未答的学生',item.record.noanswer.students):''">
+          <p @click="item.record.noanswer.count>0?toggleAllCorrec('未答的学生',item.record.noanswer.students):''">
             <span>{{item.record.noanswer?item.record.noanswer.count:0}}</span><span>未答</span>
           </p>
         </van-col>
