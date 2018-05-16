3a285eab59c1570f8ed62e17c1996a1a20446418
rigeman <283082036@qq.com>
2018-05-16 20:04:13 +0800

    rigeman

--- src/axios/iclass.js
+++ src/axios/iclass.js
@@ -2,12 +2,12 @@ import API from '@/axios/_api'
 import config from '@/config/index'
 
 let apis = {
-  iclassLiveTeacher: config.old + '/api/?q=json/ebag/mutual/iclassLiveTeacher&'
+  iclassLiveTeacher: config.apiDomain.old + '/api/?q=json/ebag/mutual/iclassLiveTeacher&'
 }
 
 let iclass = new API(apis)
 
 iclass.judgeIclassLive = (params) => {
-  return iclass.fetch(iclass.apis.iclassLiveTeacher, params, 'get')
+  return iclass.fetch(iclass.apis.iclassLiveTeacher, params, 'get', {api: 'old'})
 }
 export default iclass
