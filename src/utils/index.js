a369fbf8aabc1a072f65d539abb1fde9c095749e
rigeman <283082036@qq.com>
2018-05-21 17:44:43 +0800

    键盘和滑动组件

--- src/utils/index.js
+++ src/utils/index.js
@@ -29,7 +29,60 @@ function decrypt (theText) {
   }
   return output
 }
+// 判断元素是否含有某个className
+function hasClass(el, className) {
+  let reg = new RegExp('(^|\\s)' + className + '(\\s|$)')
+  return reg.test(el.className)
+}
+// 给某个元素添加某个className
+function addClass(el, className) {
+  if (hasClass(el, className)) {
+    return
+  }
+  let newClass = el.className.split(' ')
+  newClass.push(className)
+  el.className = newClass.join(' ')
+}
+// 移除某个className
+function removeClass(el, className) {
+  if (!hasClass(el, className)) {
+    return
+  }
+  let reg = new RegExp('(^|\\s)' + className + '(\\s|$)', 'g')
+  el.className = el.className.replace(reg, ' ')
+}
+// 获取元素上的data-属性
+function getData(el, name, val) {
+  let prefix = 'data-'
+  if (val) {
+    return el.setAttribute(prefix + name, val)
+  }
+  return el.getAttribute(prefix + name)
+}
+// 获取元素的盒模型最终值
+function getRect(el) {
+  if (el instanceof window.SVGElement) {
+    let rect = el.getBoundingClientRect()
+    return {
+      top: rect.top,
+      left: rect.left,
+      width: rect.width,
+      height: rect.height
+    }
+  } else {
+    return {
+      top: el.offsetTop,
+      left: el.offsetLeft,
+      width: el.offsetWidth,
+      height: el.offsetHeight
+    }
+  }
+}
 export default {
   encrypt,
-  decrypt
+  decrypt,
+  hasClass,
+  removeClass,
+  getData,
+  getRect
 }
