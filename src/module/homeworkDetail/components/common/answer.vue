95bc6757fabbcd68edd5fcd70f9ddfcb7e57b11f
tangxiaohui <912820339@qq.com>
2018-05-24 15:15:52 +0800

    接入接口

--- src/module/homeworkDetail/components/common/answer.vue
+++ src/module/homeworkDetail/components/common/answer.vue
@@ -1,80 +1,91 @@
 <template>
-  <van-popup v-model="answer" class="urge-popup">
-        <div class="urge-container">
-          <p>公布答案后，学生完成作答后可查看题目答案和解析，是否公布答案?</p>
-          <div class="btn-group">
-            <p class="cancel" @click="toggle">取消</p>
-            <p class="confirm" @click="confirm">确认</p>
-          </div>
-        </div>
-        </van-popup>
+  <van-popup v-model="answer" class="urge-popup" @click-overlay="toggle">
+    <div class="urge-container">
+      <p>公布答案后，学生完成作答后可查看题目答案和解析，是否公布答案?</p>
+      <div class="btn-group">
+        <p class="cancel" @click="toggle">取消</p>
+        <p class="confirm" @click="confirm">确认</p>
+      </div>
+    </div>
+  </van-popup>
 </template>
 <script>
 import homeworkDetil from "../../axios/detail.js";
-export default{
-  name: 'answer',
-  props: ['publishId', 'classID', 'teacherId'],
+export default {
+  name: "answer",
+  props: {
+    publishId: {
+      type: String
+    },
+    classId: {
+      type: String
+    },
+    teacherId: {
+      type: String
+      // default: JSON.parse(window.localStorage.userinfo).userid
+    }
+  },
   data() {
     return {
       answer: true
-    }
+    };
   },
   methods: {
     toggle() {
-      this.$emit('toggle')
+      this.$emit("toggle");
     },
     confirm() {
       let params = {
-        'publish_id': this.publishId,
-        'class_id': this.classID,
-        'teacher_id': this.teacherId
-      }
-      // homeworkDetil.sendAnswer(params).then((r) => {
-      //   console.log(r)
-      // })
-      this.toggle()
+        publish_id: this.publishId,
+        class_id: this.classID,
+        teacher_id: this.teacherId || JSON.parse(window.localStorage.userinfo).userid
+      };
+      homeworkDetil.sendAnswer(params).then(r => {
+        this.$emit("toggle", 1);
+        // console.log(r);
+      });
     }
   }
-}
+};
 </script>
 <style scoped>
-  .urge-popup {
-    width: calc(100vw - 85px);
-    border-radius: 10px;
-    padding: 30px;
-    box-sizing: border-box;
-  }
-  .urge-container>p{
-    line-height: 25px;
-     text-indent: 2rem;
-  }
-  .btn-group{
-    display: flex;
-    justify-content: space-between;
-    margin-top: 30px;
-  }
-  .btn-group>p{
-    font-size: 17px;
-    box-sizing: border-box;
-    width:100px;
-    height: 50px;
-    line-height: 48px;
-    text-align: center;
-    background: #fff;
-    color:#08b783;
-    border:1px solid #08b783;
-    border-radius: 13px;
-  }
-  .btn-group>p:first-child:active{
-    background: #f3fff8;
-    color:#8cd3be;
-    border-color:#8cd9c2;
-  }
-  .btn-group>p.confirm{
-    background: #08b783;
-    color:#fff;
-  }
-  .btn-group>p.confirm:active{
-    opacity: 0.5;
-  }
+.urge-popup {
+  width: calc(100vw - 85px);
+  border-radius: 10px;
+  padding: 30px;
+  box-sizing: border-box;
+}
+.urge-container > p {
+  line-height: 25px;
+  text-indent: 2rem;
+}
+.btn-group {
+  display: flex;
+  justify-content: space-between;
+  margin-top: 30px;
+}
+.btn-group > p {
+  font-size: 17px;
+  box-sizing: border-box;
+  width: 100px;
+  height: 50px;
+  line-height: 48px;
+  text-align: center;
+  background: #fff;
+  color: #08b783;
+  border: 1px solid #08b783;
+  border-radius: 13px;
+}
+.btn-group > p:first-child:active {
+  background: #f3fff8;
+  color: #8cd3be;
+  border-color: #8cd9c2;
+}
+.btn-group > p.confirm {
+  background: #08b783;
+  color: #fff;
+}
+.btn-group > p.confirm:active {
+  opacity: 0.5;
+}
 </style>
