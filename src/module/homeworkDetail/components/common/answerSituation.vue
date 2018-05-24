7e6ba7b20bbf49c21ed5d87ec62b4e9d878c5cda
rigeman <283082036@qq.com>
2018-05-24 15:30:10 +0800

    report

--- src/module/homeworkDetail/components/common/answerSituation.vue
+++ src/module/homeworkDetail/components/common/answerSituation.vue
@@ -1,6 +1,23 @@
 <template>
   <div id="answer-situation">
-    答案情况
+    <div class="list">
+      <div class="list-title">
+        <div class="list-row">
+          <div class="list-cell">序号</div>
+          <div class="list-cell question-content">作业内容</div>
+          <div class="list-cell">正确率</div>
+          <div class="list-cell">用时</div>
+        </div>
+      </div>
+      <div class="list-body">
+        <div class="list-row van-hairline--bottom">
+          <div class="list-cell">1</div>
+          <div class="list-cell question-content">惠顾 对对对 </div>
+          <div class="list-cell">99%</div>
+          <div class="list-cell">1'99''</div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
@@ -10,4 +27,29 @@ export default {
 }
 </script>
 <style lang="scss" scoped>
+#answer-situation{
+  height: 100%;
+  overflow-y: scroll;
+  .list{
+    padding: 0 13px;
+    font-size: 16px;
+    .list-title{
+      color: #848484;
+    }
+    .list-body{
+      color: #1e1e1e;
+    }
+    .list-row{
+      display: flex;
+      height: 45px;
+      line-height: 45px;
+    }
+    .list-cell{
+      flex: 1;
+      &.question-content{
+        flex: 2.5;
+      }
+    }
+  }
+}
 </style>
