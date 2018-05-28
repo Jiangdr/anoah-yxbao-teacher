bcdfa646662ab60c807b076e00d4637f2c854cd4
tangxiaohui <912820339@qq.com>
2018-05-28 15:13:28 +0800

    纠错页面样式

--- src/module/correction/components/correction.vue
+++ src/module/correction/components/correction.vue
@@ -1,71 +1,47 @@
 <template>
 <div class="correction">
-  <div class="title border-bottom-1px">
-    <span
-      class="back"
-      @click="goBack"
-    >
-      <van-icon name="arrow-left"></van-icon>
-      <span>返回</span>
-      </span>
-      <span class="text">纠错</span>
+  <div class="title">
+     <header-bar @back="goBack">
+        <div slot="title-name">
+          <div>纠错</div>
+        </div>
+      </header-bar>
   </div>
   <div class="classify">
     <div>
-      <span
-        v-for="(item,index) in classify"
-        :key="index"
-        :class="{'checked':isChecked(item)}"
-        @click="checkItem(item)"
-      >
+      <span v-for="(item,index) in classify" :key="index" :class="{'checked':isChecked(item)}" @click="checkItem(item)">
         {{item}}
-        </span>
+      </span>
     </div>
   </div>
   <div class="describe">
-        <!-- <textarea ></textarea> -->
-        <textarea
-          name="describe"
-          v-model="msg"
-          placeholder="请描述错误内容，我们会尽快处理"
-          :maxlength=maxLength
-        ></textarea>
+    <textarea name="describe" v-model="msg" placeholder="请描述错误内容，我们会尽快处理" :maxlength=maxLength></textarea>
   </div>
   <div class="upload">
-    <van-row class="total">
-      <van-col
-        span="22"
-        offset="1"
-      >
-        上传问题截图<span class="num">（<b>{{imgs.length}}</b>/8）</span>
-        </van-col>
-    </van-row>
-    <van-row>
-      <van-col
-        offset="1"
-        span="2"
-      >
-        <i
-          class="cubeic-alert"
-          @click="togglePopup"
-        ></i>
-          </van-col>
-    </van-row>
+    <div class="total">
+      <div>
+        上传问题截图<span class="num">（<b style="color:#4e4e50;">{{imgs.length}}</b>/8）</span>
+      </div>
+    </div>
+    <div class="container">
+      <div class="item" v-for="(img,index) in imgs" :key="index">
+        <span class="close"></span>
+        <img :src="img.src" alt="">
+      </div>
+      <div class="item upload" @click="togglePopup">
+        <img class="up" :src="imgUrl"/>
+        <p>
+          <template v-if="imgs.length===0">点击上传</template>
+          <template v-else-if="imgs.length>0">继续上传</template>
+        </p>
+      </div>
+    </div>
   </div>
-  <cube-popup
-    type="my-popup"
-    :center="false"
-    ref="myPopup"
-    v-show="this.showPopup"
-    @mask-click="togglePopup"
-  >
+  <cube-popup type="my-popup" :center="false" ref="myPopup" v-show="this.showPopup" @mask-click="togglePopup" >
     <div class="popupWrapper">
-      <div class="border-bottom-1px popupItem">拍照</div>
-      <div class="border-bottom-1px popupItem">从相册获取</div>
-      <div
-        class="border-bottom-1px popupItem"
-        @click="togglePopup"
-      >取消</div>
+      <div class="popupItem">拍照</div>
+      <div class="popupItem">从相册获取</div>
+      <div class="popupItem" @click="togglePopup">取消</div>
 </div>
 
 </cube-popup>
@@ -77,6 +53,7 @@
 </template>
 
 <script>
+import headerBar from '@/components/headerBar.vue'
 export default {
   name: 'correction',
   data() {
@@ -98,7 +75,11 @@ export default {
     }
   },
   created() {},
-  computed: {},
+  computed: {
+    imgUrl() {
+      return require('@/assets/images/correction/up.png')
+    }
+  },
   methods: {
     goBack() {
       this.$router.go(-1)
@@ -116,6 +97,9 @@ export default {
         this.checked.splice(this.checked.indexOf(item), 1)
       }
     }
+  },
+  components: {
+    headerBar
   }
 }
 </script>
@@ -124,40 +108,9 @@ export default {
 .correction {
   height: 100vh;
 }
-
-.correction>.title {
-  text-align: center;
-  line-height: 50px;
-  height: 50px;
-  padding: 0 13px;
-}
-
-.correction>.title .back {
-  display: inline-block;
-  float: left;
-}
-
-.correction>.title .text {
-  display: inline-block;
-  width: calc(100% - 100px);
-  font-weight: 600;
-}
-
-.correction>.title p {
-  font-weight: bold;
-}
-
-.correction>.title>span {
-  float: left;
-}
-
-.correction>.title span i {
-  top: 2px;
-}
-
 .correction>.classify {
   text-align: center;
-  padding: 18px 13px 0px;
+  padding: 20px 13px 0px;
 }
 
 .correction>.classify>div{
@@ -168,19 +121,20 @@ export default {
   display: inline-block;
   width:110px;
   height: 40px;
-  flex:auto;
+  flex:0 1 auto;
   line-height: 40px;
   border-radius: 5px;
-  background: rgb(246,247,249);
-  margin-right: 9px;
-  margin-bottom: 18px;
+  background: #f5f7f8;
+  font-size: 17px;
+  margin-right: 9.5px;
+  margin-bottom: 10px;
 }
 .correction>.classify>div>span:nth-child(3),
 .correction>.classify>div>span:nth-child(6){
   margin-right: 0px;
 }
 .correction>.classify>div>span.checked{
-  background: rgb(118,189,255);
+  background: #08b783;
   color:#fff;
 }
 .correction>.describe{
@@ -194,13 +148,19 @@ export default {
   border-radius: 5px;
   box-sizing: border-box;
   padding: 10px;
-  border:1px solid rgb(231,235,238);
+  border:1px solid #e8ebee;
 }
 
 .correction>.describe ::placeholder {
   color: #ccc;
 }
-
+.correction>.upload{
+  padding:0 13px;
+  box-sizing: border-box;
+  height: calc(100% - 45px - 170px - 150px - 10px - 50px - 10px - 8px);
+  border-bottom: 1px solid #e8ebee;
+  margin-bottom: 7px;
+}
 .correction>.upload>.total {
   margin: 8px 0;
 }
@@ -211,29 +171,63 @@ export default {
 .correction>.upload>.total span.num{
   color:rgb(128,129,131);
 }
-.correction .popupItem {
-  line-height: 50px;
+.correction>.upload>.container{
+  display: flex;
+}
+.correction>.upload>.container>.item{
+  flex:0 1 auto;
+  width:110px;
+  height: 110px;
   text-align: center;
+  vertical-align: middle;
+  background: #f5f7f8;
+  padding:15px;
+  box-sizing: border-box;
 }
-
+.correction>.upload>.container>.item>img.up{
+  margin-top:5px;
+  margin-bottom: 10px;
+}
+.correction>.upload>.container>.item.upload p{
+  color:#c8c9c9;
+}
 .correction .popupWrapper {
-  background: #fff;
+  margin: 0 13px 8px;
+  font-size: 16px;
+  color:#1e1e1e;
 }
-
-.correction .cube-popup-content {
+.correction .popupItem {
   background: #fff;
+  line-height: 48px;
+  height: 48px;
+  box-sizing: border-box;
+  text-align: center;
+  border-bottom: 1px solid #e8ebee;
 }
-
+.correction .popupItem:first-child {
+  border-top-left-radius: 13px;
+  border-top-right-radius: 13px;
+}
+.correction .popupItem:nth-child(2) {
+  border-bottom-left-radius: 13px;
+  border-bottom-right-radius: 13px;
+}
+.correction .popupItem:last-child{
+  margin-top:15px;
+  border-radius: 13px;
+}
 .correction>.submitBtn {
   position: fixed;
-  bottom: 34px;
-  width: calc(100% - 75px);
-  left:calc(75px / 2);
-  border-radius: 10px;
+  bottom: 8px;
+  left: 50%;
+  transform: translateX(-50%);
+  width:180px;
+  border-radius: 13px;
   height: 50px;
   line-height: 50px;
   text-align: center;
-  background: rgb(11,189,137);
+  background: #08b783;
   color:#fff;
+  font-size: 17px;
 }
 </style>
