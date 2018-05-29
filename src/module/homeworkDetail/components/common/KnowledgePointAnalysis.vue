95be6005b8a5c9e9a8fe734d5b7ddcad131c1628
rigeman <283082036@qq.com>
2018-05-29 14:57:36 +0800

    知识点分析

--- src/module/homeworkDetail/components/common/KnowledgePointAnalysis.vue
+++ src/module/homeworkDetail/components/common/KnowledgePointAnalysis.vue
@@ -1,6 +1,30 @@
 <template>
   <div id="knowledge-point-analysis">
-    知识点分析
+    <div class="list" v-if="kpList.length">
+      <div class="list-title">
+        <div class="list-row">
+          <div class="list-cell question-content left">知识点</div>
+          <div class="list-cell">题目数</div>
+          <div class="list-cell">正确率</div>
+        </div>
+      </div>
+      <div class="list-body">
+        <div class="list-row van-hairline--bottom" v-for="(item, index) in kpList" :key="index" @click="linkTo(item)">
+          <div class="list-cell left question-content ellipsis">
+            <span class="type">{{item.kp_name}}</span>
+          </div>
+          <div class="list-cell">{{item.qti_count}}</div>
+          <div class="list-cell">{{Math.round(item.class_right_rate * 100) + '%'}}</div>
+          <div class="arrow">
+            <i class="fa fa-angle-right"></i>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div v-else class="no-data">
+      <img :src="imgUrl" alt="">
+      <span style="color:#1e1e1e;font-weight:600;margin-bottom:10px;">无知识点正确率相关数据~</span>
+    </div>
   </div>
 </template>
 <script>
@@ -10,16 +34,77 @@ export default {
   name: 'knowledgePointAnalysis',
   props: ['publishId'],
   data() {
-    return {}
+    return {
+      kpList: []
+    }
+  },
+  computed: {
+    imgUrl() {
+      return require('@/assets/images/homeworkDetail/no-data.png')
+    }
   },
   mounted() {
     api.kp({
       publish_id: this.publishId
     }).then(succ => {
-      console.log(succ)
+      this.kpList = succ
     })
+  },
+  methods: {
+    linkTo(item) {
+      this.$router.push({path: `/knowledgePointDetail/${this.publishId}/${item.kp_id}/${item.qti_count}/${item.class_right_rate}`})
+    }
   }
 }
 </script>
 <style lang="scss" scoped>
+#knowledge-point-analysis{
+  width: 100%;
+  height: 100%;
+  overflow-y: scroll;
+  .list{
+    padding: 0 13px;
+    font-size: 16px;
+    .list-title{
+      color: #848484;
+    }
+    .list-body{
+      color: #1e1e1e;
+    }
+    .list-row{
+      display: flex;
+      height: 45px;
+      line-height: 45px;
+    }
+    .list-cell{
+      flex: 1;
+      text-align: center;
+      &.question-content{
+        flex: 2.5;
+        padding: 0 5px;
+      }
+      &.left{
+        text-align: left;
+      }
+    }
+  }
+  .type{
+    color: #4d4d4d
+  }
+  .no-data{
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    font-size: 20px;
+    padding: 0 26px;
+    text-align: center;
+    img{
+      width: 100px;
+      height: 130px;
+    }
+
+  }
+}
 </style>
--- src/module/homeworkDetail/router/index.js
+++ src/module/homeworkDetail/router/index.js
@@ -1,8 +1,16 @@
 import homeworkDetail from '@/module/homeworkDetail/components/index.vue'
-
-export default [{
-  path: '/homeworkDetail/:publishId/:classId',
-  name: 'homeworkDetail',
-  component: homeworkDetail,
-  meta: { requiresAuth: false, keepAlive: true }
-}]
+import knowledgePointDetail from '@/module/homeworkDetail/components/knowledgePointDetail.vue'
+export default [
+  {
+    path: '/homeworkDetail/:publishId/:classId',
+    name: 'homeworkDetail',
+    component: homeworkDetail,
+    meta: { requiresAuth: false, keepAlive: true }
+  },
+  {
+    path: '/knowledgePointDetail/:publishId/:kp_id/:qti_count/correct_rate',
+    name: 'knowledgePointDetail',
+    component: knowledgePointDetail,
+    meta: { requiresAuth: false, keepAlive: true }
+  }
+]
