c08d300db61533644f968e4e6441a665fee56645
tangxiaohui <912820339@qq.com>
2018-05-30 14:46:03 +0800

    分享

--- src/module/questionDetail/axios/getQuestionStatistics.js
+++ src/module/questionDetail/axios/getQuestionStatistics.js
@@ -1,12 +1,15 @@
 import API from '@/axios/_api'
 
 let apis = {
-  'statistics': 'jwt/homework/stat/getByRid' // 单题统计
+  'statistics': 'jwt/homework/stat/getByRid', // 单题统计
+  'share': 'jwt/user/share/add'
 }
 
 let questionStatistics = new API(apis)
 questionStatistics.getinfo = (params) => {
   return questionStatistics.fetch(questionStatistics.apis.statistics, params, 'post')
 }
-
+questionStatistics.share = (params) => {
+  return questionStatistics.fetch(questionStatistics.apis.share, params, 'post')
+}
 export default questionStatistics
