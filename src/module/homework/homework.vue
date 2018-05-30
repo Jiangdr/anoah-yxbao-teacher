4abb538d3af630683484682d77aad449c2561f1b
puronglong <puronglong@foxmail.com>
2018-05-30 18:06:20 +0800

    重置页数

--- src/module/homework/homework.vue
+++ src/module/homework/homework.vue
@@ -222,6 +222,7 @@ export default {
   methods: {
     chooseItem(e, type, value) {
       this.activeItem = 0;
+      this.currentPage = 1;
       switch (type) {
         case "time":
           if (value === 3) {
