ba923cc97dff8281446547e982e5f8ff66c551a3
tangxiaohui <912820339@qq.com>
2018-06-04 12:18:05 +0800

    base64转URL

--- src/module/correction/axios/correction.js
+++ src/module/correction/axios/correction.js
@@ -1,7 +1,9 @@
 import API from '@/axios/_api'
 
 let apis = {
-  'create': 'jwt/resource/correction/create'
+  'create': 'jwt/resource/correction/create',
+  'getAuthority': 'jwt/cuoti/error/upload_auth',
+  'upLoad': 'jwt/cuoti/error/base64_upload'
 }
 
 let correction = new API(apis)
@@ -9,5 +11,10 @@ let correction = new API(apis)
 correction.create = (params) => {
   return correction.fetch(correction.apis.create, params, 'post')
 }
-
+correction.getAuthority = (params) => {
+  return correction.fetch(correction.apis.getAuthority, params, 'post')
+}
+correction.upload = (params) => {
+  return correction.fetch(correction.apis.upLoad, params, 'post')
+}
 export default correction
--- src/module/correction/components/correction.vue
+++ src/module/correction/components/correction.vue
@@ -27,9 +27,9 @@
     <div class="container">
       <div class="item" v-for="(img,index) in imgs" :key="index">
         <span class="close" @click="remove(index)"></span>
-        <img src="" alt="" width="100%" height="100%">
+        <img :src="img" alt="" width="100%" height="100%">
       </div>
-      <div class="item upload" @click="togglePopup">
+      <div class="item upload" @click="togglePopup" v-show="this.imgs.length<maxImageNum">
         <img class="up" src="../../../assets/images/correction/up.png"/>
         <p>
           <template v-if="imgs.length===0">点击上传</template>
@@ -40,17 +40,17 @@
   </div>
   </div>
 
-  <cube-popup type="my-popup" :center="false" ref="myPopup" v-show="this.showPopup" @mask-click="togglePopup" >
+  <van-popup type="my-popup" position="bottom" v-model="showPopup" class="popup">
     <div class="popupWrapper">
-      <div class="popupItem">拍照</div>
-      <div class="popupItem">从相册获取</div>
+      <div class="popupItem" @click="capturePhoto">拍照</div>
+      <div class="popupItem" @click="selectImagesFromLocal">从相册获取</div>
       <div class="popupItem" @click="togglePopup">取消</div>
 </div>
 
-</cube-popup>
+</van-popup>
   <div class="submitBtn" @click="commit" :class="{disable:!checked.length&& !msg.length}">
     <!-- <van-button type="primary" bottom-action>提交</van-button> -->
-    <template v-if="loading">
+     <template v-if="loading">
       <van-loading type="spinner" color="black" class="loading"/>
       提交中
     </template>
@@ -64,11 +64,12 @@
 <script>
 import headerBar from '@/components/headerBar.vue'
 import correction from '../axios/correction.js'
-import { Toast } from 'vant';
+import { Toast } from 'vant'
 export default {
   name: 'correction',
-  data() {
+  data () {
     return {
+      maxImageNum: 8,
       msg: '',
       imgs: [],
       classify: {
@@ -80,40 +81,51 @@ export default {
         15: '知识点不符',
         16: '其他'
       },
-      loading: false,
       checked: [],
+      loading: false,
       showPopup: false,
       maxLength: 100
     }
   },
-  created() {},
-  computed: {
+  activated () {
+    this.imgs = []
+    this.getAuthority()
+    this.checked = []
   },
+  computed: {},
   methods: {
-    goBack() {
+    goBack () {
       this.$router.go(-1)
     },
-    togglePopup() {
+    togglePopup () {
       this.showPopup = !this.showPopup
     },
-    isChecked(index) {
+    isChecked (index) {
       return this.checked.indexOf(index) >= 0
     },
-    checkItem(index) {
+    checkItem (index) {
       if (this.checked.indexOf(index) < 0) {
         this.checked.push(index)
       } else {
         this.checked.splice(this.checked.indexOf(index), 1)
       }
     },
-    remove(index) {
+    remove (index) {
       this.imgs.splice(index, 1)
     },
-    commit() {
+    getAuthority () {
+      let params = {
+        user_id: JSON.parse(localStorage.userinfo).userid
+      }
+      correction.getAuthority(params).then(r => {
+        this.upToken = r.mo_authority_token
+      })
+    },
+    commit () {
       if (!this.checked.length && !this.msg.length) {
         return false
       }
-      this.loading = true;
+      this.loading = true
       let params = {
         user_id: JSON.parse(localStorage.userinfo).userid,
         rsid: this.$route.params.rsid,
@@ -122,12 +134,39 @@ export default {
         images: this.imgs
       }
       correction.create(params).then(r => {
-        this.loading = false;
+        this.loading = false
         Toast('提交成功！感谢您的参与，我们会尽快审核')
         setTimeout(() => {
           this.goBack()
         }, 2000)
       })
+    },
+    capturePhoto () {
+      window.appPlug.capturePhoto(this.getImagesSuc, this.getImagesFail, true, false)
+    },
+    selectImagesFromLocal () {
+      window.appPlug.getImage(this.getImagesSuc, this.getImagesFail, true, false)
+    },
+    getImagesSuc (v) {
+      let param = {
+        user_id: JSON.parse(localStorage.userinfo).userid,
+        mo_authority_token: this.upToken,
+        image: 'data:image/png;base64,' + v
+      }
+      this.imgs.unshift('data:image/png;base64,' + v)
+      if (this.imgs.length >= this.maxImageNum) {
+        this.togglePopup()
+      }
+      correction.upload(param).then(r => {
+        console.log(r)
+        this.imgs.unshift(r.path_virtual)
+        if (this.imgs.length >= this.maxImageNum) {
+          this.togglePopup()
+        }
+      })
+    },
+    getImagesFail (e) {
+      alert(e)
     }
   },
   components: {
@@ -140,7 +179,7 @@ export default {
 .correction {
   height: 100vh;
 }
-.wrapper{
+.wrapper {
   height: calc(100% - 112px);
   overflow-y: auto;
   border-bottom: 1px solid #e8ebee;
@@ -150,15 +189,15 @@ export default {
   padding: 20px 13px 0px;
 }
 
-.correction .classify>div{
+.correction .classify > div {
   display: flex;
   flex-wrap: wrap;
 }
-.correction .classify>div>span{
+.correction .classify > div > span {
   display: inline-block;
-  width:110px;
+  width: 110px;
   height: 40px;
-  flex:0 1 auto;
+  flex: 0 1 auto;
   line-height: 40px;
   border-radius: 5px;
   background: #f5f7f8;
@@ -166,16 +205,16 @@ export default {
   margin-right: 9.5px;
   margin-bottom: 10px;
 }
-.correction .classify>div>span:nth-child(3),
-.correction .classify>div>span:nth-child(6){
+.correction .classify > div > span:nth-child(3),
+.correction .classify > div > span:nth-child(6) {
   margin-right: 0px;
 }
-.correction .classify>div>span.checked{
+.correction .classify > div > span.checked {
   background: #08b783;
-  color:#fff;
+  color: #fff;
 }
-.correction .describe{
-  padding:0 13px;
+.correction .describe {
+  padding: 0 13px;
   margin-bottom: 10px;
 }
 .correction .describe textarea {
@@ -185,74 +224,77 @@ export default {
   border-radius: 5px;
   box-sizing: border-box;
   padding: 10px;
-  border:1px solid #e8ebee;
+  border: 1px solid #e8ebee;
 }
 
 .correction .describe ::placeholder {
   color: #ccc;
 }
-.correction .upload{
-  padding:0 13px;
+.correction .upload {
+  padding: 0 13px;
   box-sizing: border-box;
   margin-bottom: 7px;
 }
-.correction .upload>.total {
+.correction .upload > .total {
   margin: 8px 0;
 }
 
-.correction .upload>.total b {
+.correction .upload > .total b {
   font-weight: normal;
 }
-.correction .upload>.total span.num{
-  color:rgb(128,129,131);
+.correction .upload > .total span.num {
+  color: rgb(128, 129, 131);
 }
-.correction .upload>.container{
+.correction .upload > .container {
   display: flex;
   flex-wrap: wrap;
 }
-.correction .upload>.container>.item{
-  flex:0 1 auto;
-  width:100px;
+.correction .upload > .container > .item {
+  flex: 0 1 auto;
+  width: 100px;
   height: 100px;
   text-align: center;
   vertical-align: middle;
   background: #f5f7f8;
-  padding:15px;
+  padding: 15px;
   box-sizing: border-box;
   margin-right: 18px;
-  margin-top:15px;
+  margin-top: 15px;
   position: relative;
 }
-.correction .upload>.container>.item:nth-child(3),
-.correction .upload>.container>.item:nth-child(6),
-.correction .upload>.container>.item:nth-child(9){
+.correction .upload > .container > .item:nth-child(3),
+.correction .upload > .container > .item:nth-child(6),
+.correction .upload > .container > .item:nth-child(9) {
   margin-right: 0;
 }
-.correction .upload>.container>.item>img.up{
-  margin-top:5px;
+.correction .upload > .container > .item > img.up {
+  margin-top: 5px;
   margin-bottom: 10px;
 }
-.correction .upload>.container>.item span.close{
+.correction .upload > .container > .item span.close {
   display: inline-block;
-  width:18px;
+  width: 18px;
   height: 18px;
-  background-image: url('../../../assets/images/public/deleteselectedimage.png');
+  background-image: url("../../../assets/images/public/deleteselectedimage.png");
   background-size: 100% 100%;
   background-position: center center;
   background-repeat: no-repeat;
   position: absolute;
   right: 0;
-  top:0;
-  transform: translate(30%,-30%);
+  top: 0;
+  transform: translate(30%, -30%);
 }
-.correction .upload>.container>.item.upload p{
-  color:#c8c9c9;
+.correction .upload > .container > .item.upload p {
+  color: #c8c9c9;
   font-size: 14px;
+}
+.correction .popup{
+  background-color: transparent;
 }
 .correction .popupWrapper {
   margin: 0 13px 8px;
   font-size: 16px;
-  color:#1e1e1e;
+  color: #1e1e1e;
 }
 .correction .popupItem {
   background: #fff;
@@ -270,8 +312,8 @@ export default {
   border-bottom-left-radius: 13px;
   border-bottom-right-radius: 13px;
 }
-.correction .popupItem:last-child{
-  margin-top:15px;
+.correction .popupItem:last-child {
+  margin-top: 15px;
   border-radius: 13px;
 }
 .correction .submitBtn {
@@ -279,16 +321,16 @@ export default {
   bottom: 8px;
   left: 50%;
   transform: translateX(-50%);
-  width:180px;
+  width: 180px;
   border-radius: 13px;
   height: 50px;
   line-height: 50px;
   text-align: center;
   background: #08b783;
-  color:#fff;
+  color: #fff;
   font-size: 17px;
 }
-.correction>.submitBtn.disable{
+.correction > .submitBtn.disable {
   opacity: 0.5;
 }
 .loading{
