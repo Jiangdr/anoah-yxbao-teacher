cf8b52535a0bb881c16b56fd7e77c47b98a0b39f
cgdreamer <iamckk@qq.com>
2018-06-07 12:04:40 +0800

    UI

--- src/module/user-center/components/modifyPhone.vue
+++ src/module/user-center/components/modifyPhone.vue
@@ -1,18 +1,32 @@
 <template>
-  <div id="modify-phone">
-    <nav-bar :title="title" :hasBack="hasBack" @historyBack="back"></nav-bar>
-    <div>手机号:{{$route.params.phone}}</div>
-    <van-field center v-model="sms" label="短信验证码" placeholder="请输入短信验证码" icon="clear" @click-icon="sms = ''" :error-message="validsms">
-      <van-button slot="button" size="small" type="primary" :disabled="smsUseable" @click="sendsms">{{content}}</van-button>
-    </van-field>
-    <van-button type="primary" size="large" :disabled="isNext" @click="next">下一步</van-button>
+  <div id="modify-phone" style="height: 100%">
+    <header-bar @back="back"><span slot="title-name">{{title}}</span></header-bar>
+    <div class="topDiv">
+      <div class="personinfodiv">
+        <img :src="avatar" alt="..." class="avatar">
+        <span>{{userInfo.nicknm}}</span>
+        <div class="cl-mark">{{$route.params.phone}}</div>
+      </div>
+      <div style="width: 100%">
+        <div class="van-hairline--bottom" style="width: 95%; margin-bottom: 10px">
+          <div class="cl-mark2 ft14" style="padding-left: 15px">验证码</div>
+          <van-field center v-model="sms" placeholder="请输入验证码" icon="clear" @click-icon="sms = ''" :error-message="validsms" style="width: 100%">
+            <van-button slot="button" size="small" type="primary" :disabled="smsUseable" @click="sendsms" style="background-color:#09B784;border:none;border-radius: 9px">{{content}}</van-button>
+          </van-field>
+        </div>
+        <span style="align-self: flex-start; padding-left: 26px" class="cl-yellow ft14">如果需要修改手机,请先进行验证</span>
+      </div>
+    </div>
+    <bottom-button-area :disabled="isNext" @click="next" buttonText="下一步"></bottom-button-area>
   </div>
 </template>
 
 <script>
 import NavBar from '@/module/user-center/components/common/navbar'
 import api from '@/module/user-center/axios/usercenter'
-import {mapGetters} from 'vuex'
+import headerBar from '@/components/headerBar.vue'
+import BottomButtonArea from '@/module/user-center/components/common/bottomButtonArea'
+import {mapGetters, mapState} from 'vuex'
 import {Toast} from 'vant'
 export default {
   name: 'ModifyPhone',
@@ -32,7 +46,15 @@ export default {
   computed: {
     ...mapGetters({
       userId: 'userCenter/userId'
-    })
+    }),
+    ...mapState({
+      userInfo(state) {
+        return state.account.userInfo
+      }
+    }),
+    avatar () {
+      return this.$store.getters['runEnv/old'] + this.userInfo.avatar
+    }
   },
   watch: {
     sms (value) {
@@ -91,10 +113,38 @@ export default {
     clearInterval(this.timer)
   },
   components: {
-    NavBar
+    NavBar,
+    headerBar,
+    BottomButtonArea
   }
 }
 </script>
 
 <style scoped>
+  .avatar{
+    width: 66px;
+    height: 66px;
+    border-radius: 50%;
+    border: 2px solid #ffffff;
+    display: inline-block;
+    margin-bottom: 10px;
+  }
+  div.topDiv
+  {
+    display: flex;
+    height: 50%;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  div.personinfodiv{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 40px;
+  }
+  .van-button--disabled
+  {
+    color: #ffffff;
+  }
 </style>
